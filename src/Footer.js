ee9b24770db93a57e3495068e1621c1404408d0c
Sabastine <toshfuture@gmail.com>
2021-06-30 17:00:58 +0100

    mobile responsiveness

--- src/Footer.js
+++ src/Footer.js
@@ -4,10 +4,7 @@ const Footer = () => {
   const developer = '<Tosh/>'
   return (
     <>
-      <footer
-        className='footer mt-auto py-3 bg-light position-absolute bottom-0 w-100'
-        style={{ bottom: '0', marginTop: '1rem' }}
-      >
+      <footer className='footer mt-auto py-3 bg-light'>
         <div className='container-fluid'>
           <h6>
             Created by{' '}
