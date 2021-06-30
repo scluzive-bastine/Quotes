446b331f29143dd4a38c62b215afb606f98d125b
Sabastine <toshfuture@gmail.com>
2021-06-30 12:39:14 +0100

    Design mockup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { Container, Row, Col, Button } from 'react-bootstrap'
+import Nav from './Nav'
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Container fluid className='pl-0 pr-0 vh-100'>
+      <Nav />
+      <Container className='align-items-center d-flex h-100'>
+        <Row className='justify-content-center'>
+          <Col md={8}>
+            <div className='rounded p-5 shadow bg-white'>
+              <p>
+                New Yorker–born and raised. Currently living in the Hague, the
+                Netherlands after stints in Paris and Amsterdam. Lover of
+                travel, adventure, nature, city, dresses, and cats.
+              </p>
+            </div>
+            <div className='d-flex justify-content-center'>
+              <Button
+                variant='primary'
+                className='mt-4'
+                style={{ backgroundColor: '#282E67', borderColor: '#282E67' }}
+              >
+                New Quote
+              </Button>
+            </div>
+          </Col>
+        </Row>
+      </Container>
+    </Container>
+  )
 }
 
-export default App;
+export default App
